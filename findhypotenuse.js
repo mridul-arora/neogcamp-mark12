96766cffa20c171ffb6300ec185cf00a5ab9ab80
MRIDUL ARORA <aroramridul18@gmail.com>
2022-12-04 12:22:38 +0530

    chore: added hypotenuse

---
+++ findhypotenuse.js
@@ -0,0 +1,31 @@
+//common text fields
+const outputText = document.querySelector("#outputText");
+const outputHint = document.querySelector("#outputHint");
+
+//common functions 
+function showText(text) {
+    outputText.innerText = text;
+}
+
+function showHint(text) {
+    outputHint.innerText = text;
+}
+
+function hideHint() {
+    outputHint.style.display = "none";
+}
+
+// input: lengths, Calculate the hypotenuse length..
+const length = document.querySelectorAll(".length");
+const calcHypoBtn = document.querySelector("#btn-calculateHypote");
+
+calcHypoBtn.addEventListener("click", clickEventHandler);
+
+function clickEventHandler(){
+    const a = Number(length[0].value);
+    const b = Number(length[1].value);
+    const hypoSquare = ((a*a)+(b*b));
+    const hypo = Math.sqrt(hypoSquare);
+    showText("The length of Hypotenuse is: " + hypo);
+};
+
